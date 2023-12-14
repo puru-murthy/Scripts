69b99e4ecc44d3780e0ad3a5e227e80978addf0c
puru-murthy <kpurshi@gmail.com>
2023-12-14 16:23:22 -0600

    Added python dir

---
+++ python/Pass_generate.py
@@ -0,0 +1,16 @@
+# Generate password which is tough to break
+import string
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "@#$%&*/\?"
+
+Use_for = lower_case + upper_case + number + symbols
+length_for_pass = 8
+
+password = "".join(random.sample(Use_for, length_for_pass))
+
+print("Your Generated Password is :",password)
+
